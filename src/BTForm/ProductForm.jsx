f59862cca8c8496b423d4c33e1f4b11d3457ace4
binhnguyen <leechiupin@gmail.com>
2023-10-28 22:58:31 +0900

    Sửa các thành phần chung gom lên thành 1 hàm chung

--- src/BTForm/ProductForm.jsx
+++ src/BTForm/ProductForm.jsx
@@ -12,17 +12,25 @@ export const ProductForm = () => {
 
     // để lấy dữ liệu trên ô input cần thêm onChange
     // mỗi sự kiện trong JS đề trả về 1 biến event
-    const handelFormValue = (e, name) => {
-        // e là event, name là cái tên của thuộc tính
-        setFormValue({
-            // giữ lại các thuộc tính trước đó đã xét ko thì sẽ mất 
+    // const handelFormValue = (e, name) => {
+    //     // e là event, name là cái tên của thuộc tính
+    //     setFormValue({
+    //         // giữ lại các thuộc tính trước đó đã xét ko thì sẽ mất 
+    //         ...formValue,
+    //         // console.log("event.target.value", e.target.value);
+    //         // sau khi lấy đc value lại rồi thì xét lại thuộc tính
+    //         [name]: e.target.value,
+    //     })
+    // }
+
+    // hàm currying funtion trả về 1 cái hàm khác
+    // cách viết tắt bỏ return
+    const handelFormValue = (name) => (e) => {
+        setFormValue({ 
             ...formValue,
-            // console.log("event.target.value", e.target.value);
-            // sau khi lấy đc value lại rồi thì xét lại thuộc tính
             [name]: e.target.value,
         })
     }
-
     return (
         // ở đây đặt là form, button đặt trong form nếu ko có type thì mặc định là submit
         // button có type là submit thì khi click vào sẽ chạy vào hàm onSubmit của form
@@ -32,36 +40,28 @@ export const ProductForm = () => {
                 <div className='col-6'>
                     <div className='mt-3'>
                         <label htmlFor="">Mã sinh viên</label>
-                        <input type="text" name="" id="" className="form-control" 
-                            onChange={(e) => 
-                                handelFormValue(e,"id")
-                            }
+                        <input type="text" name="" id="" className="form-control"
+                            onChange={handelFormValue("id")}
                         />
                     </div>
                     <div className='mt-3'>
                         <label htmlFor="">Số điện thoại</label>
-                        <input type="number" name="" id="" className="form-control" 
-                            onChange={(e) => 
-                                handelFormValue(e,"phone")
-                            }
+                        <input type="number" name="" id="" className="form-control"
+                            onChange={handelFormValue("phone")}
                         />
                     </div>
                 </div>
                 <div className='col-6'>
                     <div className='mt-3'>
                         <label htmlFor="">Họ tên</label>
-                        <input type="text" name="" id="" className="form-control" 
-                            onChange={(e) => 
-                                handelFormValue(e,"name")
-                            }
+                        <input type="text" name="" id="" className="form-control"
+                            onChange={handelFormValue("name")}
                         />
                     </div>
                     <div className='mt-3'>
                         <label htmlFor="">Email</label>
-                        <input type="email" name="" id="" className="form-control" 
-                            onChange={(e) => 
-                                handelFormValue(e,"mail")
-                            }
+                        <input type="email" name="" id="" className="form-control"
+                            onChange={handelFormValue("mail")}
                         />
                     </div>
                 </div>
