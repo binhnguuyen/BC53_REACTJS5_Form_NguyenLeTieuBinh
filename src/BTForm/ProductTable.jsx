4d3509fbfe702bc21560d24b1a19334907102916
binhnguyen <leechiupin@gmail.com>
2023-10-29 13:18:05 +0900

    Thêm key khi map

--- src/BTForm/ProductTable.jsx
+++ src/BTForm/ProductTable.jsx
@@ -21,14 +21,18 @@ export const ProductTable = () => {
                 {
                     productList.map( (product) => {
                         return (
-                            <tr>
+                            <tr key={product.id}>
                                 <td>{product.id}</td>
                                 <td>{product.name}</td>
                                 <td>{product.phone}</td>
                                 <td>{product.mail}</td>
                                 <td>
                                     <button className='btn btn-warning'><i class="fa fa-edit"></i></button>
-                                    <button className='btn btn-danger ms-3'><i class="fa fa-trash-alt"></i></button>
+                                    <button className='btn btn-danger ms-3'
+
+                                    >
+                                        <i class="fa fa-trash-alt"></i>
+                                    </button>
                                 </td>
                             </tr>
                         )
