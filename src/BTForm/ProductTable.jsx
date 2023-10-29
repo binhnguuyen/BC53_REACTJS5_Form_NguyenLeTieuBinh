2eb65c671b798311ca6f6d7ad7ba3fd90ac8c241
binhnguyen <leechiupin@gmail.com>
2023-10-29 13:28:46 +0900

    Thêm tính năng delete

--- src/BTForm/ProductTable.jsx
+++ src/BTForm/ProductTable.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { btFormActions } from '../store/BTForm/slice'
 
 export const ProductTable = () => {
     const { productList } = useSelector(state => state.btForm)
     // console.log('productList: ', productList);
+
+    // Thêm dispatch
+    const dispatch = useDispatch()
+
   return (
     <div>
         <h2 className='p-3 bg-dark text-white mt-5'>Danh sách sinh viên</h2>
@@ -27,11 +32,14 @@ export const ProductTable = () => {
                                 <td>{product.phone}</td>
                                 <td>{product.mail}</td>
                                 <td>
-                                    <button className='btn btn-warning'><i class="fa fa-edit"></i></button>
+                                    <button className='btn btn-warning'><i className="fa fa-edit"></i></button>
                                     <button className='btn btn-danger ms-3'
-
+                                        onClick={() => {
+                                            // delete thì chỉ cần đưa cái id lên store là OK
+                                            dispatch(btFormActions.deleteProduct(product.id))
+                                        }}
                                     >
-                                        <i class="fa fa-trash-alt"></i>
+                                        <i className="fa fa-trash-alt"></i>
                                     </button>
                                 </td>
                             </tr>
